b84604fbe714e9dcaea6fb93176905d9dda95584
Matt Dawson <matthewtdawson@gmail.com>
2023-07-11 22:09:21 -0400

    Give canvas a chance to load

--- module/main.mjs
+++ module/main.mjs
@@ -4,7 +4,9 @@ Hooks.once("init", onInit);
 
 Hooks.once("ready", onReady);
 
-Hooks.once("canvasInit", canvas.simplefog.canvasInit);
+Hooks.once("canvasInit", () => {
+    canvas.simplefog.canvasInit();
+});
 
 Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => {
     registerPackageDebugFlag("simplefog");
