f495c0af66de5866ff0fda7962b843a9af5717d4
Matt Dawson <matthewtdawson@gmail.com>
2023-07-14 11:49:10 -0400

    Standardize Hook management

--- module/main.mjs
+++ module/main.mjs
@@ -1,8 +1,16 @@
-import { onInit, onReady } from "./js/setup.mjs";
+import { onInit, onReady } from "./js/load.mjs";
+import {
+    onGetSceneControlButtons,
+    onRenderSceneControls
+} from "./js/controls.mjs";
 
 Hooks.once("init", onInit);
 
 Hooks.once("ready", onReady);
+
+Hooks.on("getSceneControlButtons", onGetSceneControlButtons);
+
+Hooks.on("renderSceneControls", onRenderSceneControls);
 
 Hooks.once("canvasInit", () => {
     canvas.simplefog.canvasInit();
