ce35989771a15135a66937e975d5e97daea5e17f
Matt Dawson <matthewtdawson@gmail.com>
2023-07-26 22:50:41 -0400

    Add deploy-local script

---
+++ deploy-local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    exit 1
+else
+    TARGET_DIR="$1"
+    ORIGIN_DIR=`pwd`
+    npm run package
+    mv build.zip $TARGET_DIR
+    cd $TARGET_DIR
+    unzip -o build.zip
+    cd $ORIGIN_DIR
+fi
